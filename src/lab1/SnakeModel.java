f104cce77d15c33ed1790614198331738391eddb
agnesmardh <agnes.mardh@gmail.com>
2016-11-08 09:12:10 +0100

    1.0

---
+++ src/lab1/SnakeModel.java
@@ -0,0 +1,13 @@
+package lab1;
+
+/**
+ * Created by agnesmardh on 2016-11-08.
+ */
+public class SnakeModel extends GameModel {
+
+
+    @Override
+    public void gameUpdate(final int lastKey) throws GameOverException {
+
+    }
+}
